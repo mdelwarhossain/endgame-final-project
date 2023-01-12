97c181c5c2363cc2247ff5ded64e6e974fc549ea
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2023-01-12 19:34:02 +0600

    statistics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,13 @@ import './App.css';
 import Footer from './Pages/Footer/Footer';
 
 import Navbar from './Pages/Navbar/Navbar';
+import Statistics from './Pages/Statistics/Statistics';
 
 function App() {
   return (
     <div className="">
       <Navbar/>
+      <Statistics/>
       <Footer/>
       
     </div>
